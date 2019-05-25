bc9bea94a60989f3bfbcb11e112c264c8f3a1a4a
David <jdduprat@gmail.com>
2019-05-24 22:05:54 -0300

    lindito

--- apps/voto/admin.py
+++ apps/voto/admin.py
@@ -15,11 +15,7 @@ class VotoAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super(VotoAdmin, self).get_queryset(request)
-
-        if request.user.has_perm('voto.can_read_assigned_schools'):
-            return qs.filter(created_by=request.user)
-        else:
-            return qs
+        return qs
 
     def save_model(self, request, obj, form, change):             
         if not change: 
@@ -43,13 +39,16 @@ def get_next_in_date_hierarchy(request, date_hierarchy):
 class LoadContractSummaryAdmin(admin.ModelAdmin):
     change_list_template = 'votes_list.html'
     actions = None
-    date_hierarchy = 'created_date'
+    
     # Prevent additional queries for pagination.
     show_full_result_count = False
 
     #list_display = ['electoral_list', 'category', 'quantity']
     list_filter = (
         'election',
+        'table__school',
+        'table',
+        'electoral_list__party',
         'category',
         'electoral_list',
     )
@@ -85,42 +84,19 @@ class LoadContractSummaryAdmin(admin.ModelAdmin):
 
         # List view
         metrics = {
-            'total': Sum('table__elctors_qty'),
             'total_votes': Sum('quantity'),
         }
 
         response.context_data['summary'] = list(
             qs
-            .values('electoral_list__party__name', 'electoral_list__name', 'category__name')
+            .values('electoral_list__party__name', 'category__name', 'electoral_list__name')
             .annotate(**metrics)
-            .order_by('-total_votes')
+            .order_by('electoral_list__party__name', 'category__name', '-total_votes')
         )
-
-        print(response.context_data['summary'])
 
         # List view summary
         response.context_data['summary_total'] = dict(qs.aggregate(**metrics))
 
-        # Chart        
-        period = get_next_in_date_hierarchy(request, self.date_hierarchy)
-        response.context_data['period'] = period
-        summary_over_time = qs.annotate(
-            period=Trunc('created_date', 'day', output_field=DateField()),
-        ).values('period').annotate(total=Sum('quantity')).order_by('period')
-
-        summary_range = summary_over_time.aggregate(
-            low=Min('total'),
-            high=Max('total'),
-        )
-        high = summary_range.get('high', 0)
-        low = summary_range.get('low', 0)
-
-        response.context_data['summary_over_time'] = [{
-            'period': x['period'],
-            'total': x['total'] or 0,
-            'pct': \
-               ((x['total'] or 0) - low) / (high - low) * 100
-               if high > low else 0,
-        } for x in summary_over_time]
+        
         
         return response
