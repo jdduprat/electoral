0679577c5adb9a566b52b11f320a1a99ef1197da
david <david.duprat@deviq.io>
2019-08-13 19:57:24 -0300

    a

--- apps/voto/admin.py
+++ apps/voto/admin.py
@@ -225,8 +225,8 @@ class VotoGraphsAdmin(admin.ModelAdmin):
             return response
 
         election = q['election__id__exact']
-        if election:
-            election = Election.objects.get(pk=q['election__id__exact'])
+        #if election:
+        #    election = Election.objects.get(pk=q['election__id__exact'])
 
         votes = Voto.objects.filter(election=election)
         other_votes = Voto.objects.filter(election=election, electoral_list__party__isnull=True)
