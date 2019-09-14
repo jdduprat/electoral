10b826bfc0dc64f7a1cbcae0f505b122571e53d0
david <david.duprat@deviq.io>
2019-09-14 20:19:00 -0300

    Cambio de contraseñas de usario

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib.admin.helpers import ActionForm
 from .models import Usuario
 from apps.places.models import School
+from django import forms
 
 class UsuarioInline(admin.StackedInline):
     model = Usuario
@@ -15,6 +17,35 @@ class SchoolInline(admin.TabularInline):
     model = School.assigned_to.through
     verbose_name_plural = 'Escuelas Asignadas'
     verbose_name = 'Escuela'
+
+
+def resetPassword(self, request, queryset):
+    count = 0
+    for obj in queryset:
+        #4 primeras letras del apellido y 3 ultimos digitos DNI
+        new_password = obj.last_name[0:4] + str(obj.usuario.dni)[5:8]
+        obj.set_password(new_password)
+        obj.save()
+        count += 1
+    self.message_user(request, "Se actualizaron %s registros" % count )
+
+resetPassword.short_description = "Crear contraseñas de los usuarios seleccionados"
+
+
+class UpdateActionForm(ActionForm): 
+    new_password = forms.CharField(label='Nueva Contraseña', max_length=30)
+
+
+def change_password(self, request, queryset):
+    new_password = request.POST['new_password']
+    count = 0
+    for obj in queryset:
+        obj.set_password(new_password)
+        obj.save()
+        count += 1
+    self.message_user(request, "Se actualizaron %s registros" % count )
+
+change_password.short_description = 'Cambiar contraseñas de los usuarios seleccionados'
 
 
 def active(self, request, queryset):
@@ -44,7 +75,8 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('groups', 'is_active', 'is_staff', 'school') 
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_dni', 'get_tel')
     list_select_related = ('usuario', )
-    actions = (active, unactive)
+    actions = (active, unactive, resetPassword, change_password)
+    action_form = UpdateActionForm
 
     def get_dni(self, instance):
             return instance.usuario.dni 
