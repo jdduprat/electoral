cc9c743ee9bff39d576a0eeee69812d7a2306acf
david duprat <david.duprat@deviq.io>
2019-10-21 20:38:17 -0300

    s

--- apps/voto/admin.py
+++ apps/voto/admin.py
@@ -266,7 +266,6 @@ class VotoGraphsAdmin(admin.ModelAdmin):
         response.context_data['election'] = election
         response.context_data['categories'] = Category.objects.filter(election=election).order_by('order_reports')
         response.context_data['votes_per_party'] = votes.exclude(electoral_list__party__isnull=True).values('category__pk', 'electoral_list__party__name', 'electoral_list__party__color').annotate(Sum('quantity')).order_by('category__pk', 'electoral_list__party__name')
-        response.context_data['other_votes'] = other_votes.filter(category__pk=cat_filter.pk).values('electoral_list__name').annotate(Sum('quantity'))
         response.context_data['other_votes_bycat'] = other_votes.values('category__pk', 'electoral_list__name').annotate(Sum('quantity')).order_by('category__pk', 'electoral_list__name')
         
         votes_bylist = votes.exclude(electoral_list__party__isnull=True).values('category__pk', 'electoral_list__name', 'electoral_list__head', 'electoral_list__party__color').annotate(Sum('quantity'))
@@ -275,7 +274,8 @@ class VotoGraphsAdmin(admin.ModelAdmin):
         response.context_data['votes_bylist'] = votes_bylist.union(other_votes_bylist).order_by('-quantity__sum')
         
         cat_filter = Category.objects.filter(election=election).first()
-
+        
+        response.context_data['other_votes'] = other_votes.filter(category__pk=cat_filter.pk).values('electoral_list__name').annotate(Sum('quantity'))
         response.context_data['totals_votes'] = votes.filter(category__pk=cat_filter.pk).aggregate(Sum('quantity'))
         response.context_data['totals_positives'] = votes.filter(category__pk=cat_filter.pk).exclude(electoral_list__party__isnull=True).aggregate(Sum('quantity'))
         response.context_data['totals_tables'] = { 'closed': tables.exclude(closed=False).count(), 'total': tables.count() }
